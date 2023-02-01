19cff6891ecff6c82ad5c6f60833e1666d62f21c
Valeria <soltanvaleria01@gmail.com>
2023-02-01 10:20:49 +0200

    Creating Service layer and controller

---
+++ src/main/java/springframework/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package springframework.spring5jokesapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.spring5jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
